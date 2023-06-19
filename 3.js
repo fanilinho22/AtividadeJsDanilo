c0e99b9c4911cb42fd262c04ae833e9d7ee2aed3
Daniel Pedrozo <102592017+fanilinho22@users.noreply.github.com>
2023-06-19 09:39:01 -0300

    x

--- 3.js
+++ 3.js
@@ -3,7 +3,7 @@ function nomeMes(n) {
     var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto",
         "Setembro", "Outubro", "Novembro", "Dezembro"]
     if (n < 1 || n > 12) {
-        return "Erro: Número inválido. O número deve estar entre 1 e 12"
+        return "O número deve estar entre 1 e 12!!!!!!"
     } else {
         var i = n - 1
         return meses[i]
